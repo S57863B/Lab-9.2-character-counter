7c80f25cd638aa4efb970adb712a7865b823ad0d
S57863B <i349cjhswe@gmail.com>
2026-04-25 18:37:46 -0400

    Added logic to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import { CharacterCounter } from './components/CharacterCounter/CharacterCounter'
 
 function App() {
 
   return (
     <>
-
+      <div className='main-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8'>
+        <CharacterCounter minWords={25} maxWords={100}/>
+      </div>
     </>
   )
 }
